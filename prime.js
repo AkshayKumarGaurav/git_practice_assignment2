0dcd1b8a4d896a11d26ce3631720e66e6a96bf0b
Akshay <akshaykumargaurav@gmail.com>
2022-11-02 12:56:42 +0530

    code for checking prime

---
+++ prime.js
@@ -0,0 +1,21 @@
+function check_prime(){
+  
+    let count=0;
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        console.log(num,"is a prime");
+    }
+    else
+    {
+        console.log(num,"is not a prime");
+    }
+  }
+  check_prime(10);
+  
